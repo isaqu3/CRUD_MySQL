5ac08d04ac46cfadef7126acaf8ff98747550bd3
isaque.santos <isaque.santos@DP-CRM18>
2021-09-03 16:32:40 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_MySQL/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace CRUD_MySQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(10, 35);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(49, 17);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.Location = new System.Drawing.Point(18, 84);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(38, 17);
+            this.lblCPF.TabIndex = 2;
+            this.lblCPF.Text = "CPF:";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(303, 36);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(25, 17);
+            this.lblID.TabIndex = 3;
+            this.lblID.Text = "ID:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(62, 34);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 4;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtID_CPF_TextChanged);
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(62, 81);
+            this.txtCPF.MaxLength = 11;
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(100, 20);
+            this.txtCPF.TabIndex = 5;
+            this.txtCPF.TextChanged += new System.EventHandler(this.txtID_CPF_TextChanged);
+            this.txtCPF.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCPF_KeyPress);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(334, 35);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 6;
+            this.txtID.TextChanged += new System.EventHandler(this.NomeCPFID_TextChanged);
+            this.txtID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtID_KeyPress);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(62, 119);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 7;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(347, 90);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 8;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(347, 61);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 9;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(347, 119);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 10;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(266, 169);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(75, 23);
+            this.btnExibir.TabIndex = 11;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(12, 198);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(433, 163);
+            this.dgvDados.TabIndex = 12;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(347, 169);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 13;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 373);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.DataGridView dgvDados;
+        public System.Windows.Forms.TextBox txtNome;
+        public System.Windows.Forms.TextBox txtCPF;
+        public System.Windows.Forms.Button btnNovo;
+        public System.Windows.Forms.Button btnExcluir;
+        public System.Windows.Forms.Button btnEditar;
+        public System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ CRUD_MySQL/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CRUD_MySQL
+{
+    public partial class Form1 : Form
+    {
+        // string de conexão com o banco MySQL
+        MySqlConnection conexao = new MySqlConnection
+                    ("Server = localhost; Database = europa; Uid = root; Pwd = 123@qwe;");
+
+        MySqlCommand comando;
+        MySqlDataAdapter da;
+        string strSQL;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void IntNumber(KeyPressEventArgs e)
+        {
+            // função para permitir somente a entrada de números em algum 
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtCPF_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // função de somente números para o campo CPF
+            IntNumber(e);
+        }
+
+        private void txtID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // função de somente números para o campo ID
+
+            IntNumber(e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // inicia o form com todos os botões que exigem campos desativados
+            btnNovo.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnEditar.Enabled = false;
+            btnConsultar.Enabled = false;
+        }
+
+        private void txtID_CPF_TextChanged(object sender, EventArgs e)
+        {
+            // Habilita o botão Novo somente se os campos Nome e CPF estiverem preenchidos
+            btnNovo.Enabled =
+                !string.IsNullOrEmpty(txtNome.Text) &&
+                !string.IsNullOrEmpty(txtCPF.Text) ? true : false;
+        }
+
+        private void NomeCPFID_TextChanged(object sender, EventArgs e)
+        {
+            // Habilita os botões Editar, Exluir e Consultar somente se o campo 
+            // ID estiver preenchido
+            btnEditar.Enabled =
+                !string.IsNullOrEmpty(txtNome.Text) &&
+                !string.IsNullOrEmpty(txtCPF.Text) &&
+                !string.IsNullOrEmpty(txtID.Text) ? true : false;
+
+            btnExcluir.Enabled =
+                !string.IsNullOrEmpty(txtID.Text) ? true : false;
+
+            btnConsultar.Enabled =
+                !string.IsNullOrEmpty(txtID.Text) ? true : false;
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            // Funcionalidade do botão NOVO com o comando INSERT
+            try
+            {
+                strSQL =
+                    "INSERT INTO tb_clientes (nome, cpf) VALUES (@NOME, @CPF)";
+
+
+                comando = new MySqlCommand(strSQL, conexao);
+                comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+                comando.Parameters.AddWithValue("@CPF", txtCPF.Text);
+
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Registro inserido com sucesso");
+
+
+            }
+            catch (Exception r)
+            {
+                MessageBox.Show(r.Message);
+            }
+            finally
+            {
+                exibeDB();
+                txtNome.Text = null;
+                txtCPF.Text = null;
+                txtID.Text = null;
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            // Funcionalidade do botão EDITAR com o comando UPDATE
+            try
+            {
+                strSQL =
+                    "UPDATE tb_clientes SET nome = @NOME, cpf = @CPF WHERE ID = @ID";
+
+
+                comando = new MySqlCommand(strSQL, conexao);
+                comando.Parameters.AddWithValue("@NOME", txtNome.Text);
+                comando.Parameters.AddWithValue("@CPF", txtCPF.Text);
+                comando.Parameters.AddWithValue("@ID", txtID.Text);
+
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Registro atualizado com sucesso");
+
+                
+                
+            }
+            catch (Exception r)
+            {
+                MessageBox.Show(r.Message);
+            }
+            finally
+            {
+                exibeDB();
+                txtNome.Text = null;
+                txtCPF.Text = null;
+                txtID.Text = null;
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            // Funcionalidade do botão EXCLUIR com o comando DELETE
+            try
+            {
+                strSQL =
+                    "DELETE FROM tb_clientes WHERE ID = @ID";
+
+
+                comando = new MySqlCommand(strSQL, conexao);
+                comando.Parameters.AddWithValue("@ID", txtID.Text);
+
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Registro excluido com sucesso");
+            }
+            catch (Exception r)
+            {
+                MessageBox.Show(r.Message);
+            }
+            finally
+            {
+                exibeDB();
+                txtNome.Text = null;
+                txtCPF.Text = null;
+                txtID.Text = null;
+
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            // Funcionalidade do botão CONSULTAR com o comando SELECT
+            try
+            {
+                strSQL =
+                    $"SELECT * FROM tb_clientes WHERE ID = {txtID.Text}";
+
+                da = new MySqlDataAdapter(strSQL, conexao);
+
+                var dt = new DataTable();
+
+                da.Fill(dt);
+
+                dgvDados.DataSource = dt;
+            }
+            catch (Exception r)
+            {
+                MessageBox.Show(r.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                strSQL = null;
+                comando = null;
+            }
+        }
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            exibeDB();
+        }
+
+        private void exibeDB()
+        {
+            // Funcionalidade de exibir toda a tabela de clientes com o comando SELECT
+            // Armazenei em uma função para poder reaproveitar nos outros campos, mostrando
+            // a tabela atualizada após criar, editar ou excluir registros
+            try
+            {
+                strSQL =
+                    "SELECT * FROM tb_clientes";
+
+
+                da = new MySqlDataAdapter(strSQL, conexao);
+
+                var dt = new DataTable();
+
+                da.Fill(dt);
+
+                dgvDados.DataSource = dt;
+            }
+            catch (Exception r)
+            {
+                MessageBox.Show(r.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                strSQL = null;
+                comando = null;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            // Funcionalidade do botão LIMPAR com o comando TRUNCATE,
+            // possui uma message box com confirmação por segurança
+            var limpaSN = MessageBox.Show
+                ("Deseja limpar a tabela de clientes?", "ATENÇÃO!", MessageBoxButtons.YesNo);
+
+            if (limpaSN == DialogResult.Yes)
+            {
+                try
+                {
+                    strSQL =
+                        "TRUNCATE tb_clientes";
+
+                    comando = new MySqlCommand(strSQL, conexao);
+
+                    conexao.Open();
+
+                    comando.ExecuteNonQuery();
+
+                    MessageBox.Show("Tabela limpa com sucesso");
+                }
+                catch (Exception r)
+                {
+                    MessageBox.Show(r.Message);
+                }
+                finally
+                {
+                    exibeDB();
+                }
+            }
+        }
+    }
+}
+
